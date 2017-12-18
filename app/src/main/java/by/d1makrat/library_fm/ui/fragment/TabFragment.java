839c8b39f4c33da58a857de427bb42e6861d65ac
DzmitryLakisau <laki_dimon@mail.ru>
2017-12-19 00:47:50 +0300

    refactoring TopAlbumsFragment

--- app/src/main/java/by/d1makrat/library_fm/ui/fragment/TabFragment.java
+++ app/src/main/java/by/d1makrat/library_fm/ui/fragment/TabFragment.java
@@ -3,6 +3,7 @@ package by.d1makrat.library_fm.ui.fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -17,6 +18,9 @@ import android.widget.Toast;
 
 import by.d1makrat.library_fm.NetworkStatusChecker;
 import by.d1makrat.library_fm.R;
+import by.d1makrat.library_fm.adapter.pages.TopAlbumsAdapter;
+import by.d1makrat.library_fm.adapter.pages.TopArtistsAdapter;
+import by.d1makrat.library_fm.adapter.pages.TopTracksAdapter;
 
 public class TabFragment extends Fragment {
 
@@ -33,7 +37,7 @@ public class TabFragment extends Fragment {
     private final String[] date_presets= {"", "?date_preset=LAST_7_DAYS", "?date_preset=LAST_30_DAYS", "?date_preset=LAST_90_DAYS", "?date_preset=LAST_180_DAYS", "?date_preset=LAST_365_DAYS"};
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         View v = inflater.inflate(R.layout.viewpager, container, false);
@@ -92,135 +96,11 @@ public class TabFragment extends Fragment {
     public boolean onContextItemSelected(MenuItem item) {
         if (NetworkStatusChecker.isNetworkAvailable()) {
             return false;
-        }
-        else {
+        } else {
             Toast toast;
             toast = Toast.makeText(getContext(), R.string.network_is_not_connected, Toast.LENGTH_SHORT);
             toast.show();
             return true;
-        }
-    }
-
-
-    public class TopAlbumsAdapter extends FragmentPagerAdapter {
-//        Context context = null;
-        private final String periods[] = new String[]{"overall", "7day", "1month", "3month", "6month",  "12month"};
-        private final String tab_names[] = new String[]{"Overall", "Week", "Month", "3 months", "6 months", "Year"};
-//        private String sessionKey = null;
-//        private String username = null;
-//        private String cachepath = null;
-
-        public TopAlbumsAdapter(FragmentManager mgr) {
-            super(mgr);
-        }
-
-        @Override
-        public int getCount() {
-            return(periods.length);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            Bundle bundle = new Bundle();
-            bundle.putString("sessionKey", sessionKey);
-            bundle.putString("username", username);
-            bundle.putString("period", periods[position]);
-            bundle.putString("cachepath", cachepath);
-            bundle.putString("resolution", resolution);
-            Fragment fragment = new TopAlbumsFragment();
-            fragment.setArguments(bundle);
-//        viewpager.setRetainInstance(true);
-//            Log.d("DEBUG", "viewpager " + position + " is showed");
-            return fragment;
-        }
-
-        @Override
-        public String getPageTitle(int position) {
-            return tab_names[position];
-        }
-    }
-
-    public class TopArtistsAdapter extends FragmentPagerAdapter {
-        //        Context context = null;
-        private final String periods[] = new String[]{"overall", "7day", "1month", "3month", "6month",  "12month"};
-        private final String tab_names[] = new String[]{"Overall", "Week", "Month", "3 months", "6 months", "Year"};
-//        private String sessionKey = null;
-//        private String username = null;
-//        private String cachepath = null;
-
-        public TopArtistsAdapter(FragmentManager mgr) {
-            super(mgr);
-//            this.context = context;
-//            sessionKey = params[0];
-//            username = params[1];
-//            cachepath = params[2];
-        }
-
-        @Override
-        public int getCount() {
-            return(periods.length);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            Bundle bundle = new Bundle();
-            bundle.putString("sessionKey", sessionKey);
-            bundle.putString("username", username);
-            bundle.putString("period", periods[position]);
-            bundle.putString("cachepath", cachepath);
-            bundle.putString("resolution", resolution);
-            Fragment fragment = new TopArtistsFragment();
-            fragment.setArguments(bundle);
-//        viewpager.setRetainInstance(true);
-//            Log.d("DEBUG", "viewpager " + position + " is showed");
-            return fragment;
-        }
-
-        @Override
-        public String getPageTitle(int position) {
-            return tab_names[position];
-        }
-    }
-
-    public class TopTracksAdapter extends FragmentPagerAdapter {
-        //        Context context = null;
-        private final String periods[] = new String[]{"overall", "7day", "1month", "3month", "6month",  "12month"};
-        private final String tab_names[] = new String[]{"Overall", "Week", "Month", "3 months", "6 months", "Year"};
-//        private String sessionKey = null;
-//        private String username = null;
-//        private String cachepath = null;
-
-        public TopTracksAdapter(FragmentManager mgr) {
-            super(mgr);
-//            this.context = context;
-//            sessionKey = params[0];
-//            username = params[1];
-//            cachepath = params[2];
-        }
-
-        @Override
-        public int getCount() {
-            return(periods.length);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            Bundle bundle = new Bundle();
-            bundle.putString("sessionKey", sessionKey);
-            bundle.putString("username", username);
-            bundle.putString("period", periods[position]);
-            bundle.putString("cachepath", cachepath);
-            bundle.putString("resolution", resolution);
-            Fragment fragment = new TopTracksFragment();
-            fragment.setArguments(bundle);
-//        viewpager.setRetainInstance(true);
-//            Log.d("DEBUG", "viewpager " + position + " is showed");
-            return fragment;
-        }
-
-        @Override
-        public String getPageTitle(int position) {
-            return tab_names[position];
         }
     }
 
