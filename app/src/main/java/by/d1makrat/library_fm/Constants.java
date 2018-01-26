7fbe77f0bf01205f1ebacee2bf22d94199692452
DzmitryLakisau <laki_dimon@mail.ru>
2018-01-27 00:32:38 +0300

    minor changes

--- app/src/main/java/by/d1makrat/library_fm/Constants.java
+++ app/src/main/java/by/d1makrat/library_fm/Constants.java
@@ -1,6 +1,12 @@
 package by.d1makrat.library_fm;
 
 public final class Constants {
+
+    private Constants(){}
+
+    public static final String ALBUM_KEY = "album";
+    public static final String ARTIST_KEY = "artist";
+    public static final String TRACK_KEY = "track";
     public static final String PERIOD_KEY = "period";
     public static final String SCROBBLES_PER_PAGE_KEY = "scrobbles_per_page";
     public static final String SCROBBLES_OF_ARTIST_TAG = "ScrobblesOfArtistFragment";
@@ -9,7 +15,28 @@ public final class Constants {
     public static final String FILTER_DIALOG_FROM_BUNDLE_KEY = "from";
     public static final String FILTER_DIALOG_TO_BUNDLE_KEY = "to";
     public static final String[] DATE_PRESETS_FOR_URL = {"", "?date_preset=LAST_7_DAYS", "?date_preset=LAST_30_DAYS", "?date_preset=LAST_90_DAYS", "?date_preset=LAST_180_DAYS", "?date_preset=LAST_365_DAYS"};
-    public static final String[] DATE_PERIODS_FOR_API = {"overall", "7day", "1month", "3month", "6month",  "12month"};
+    public static final String[] DATE_PERIODS_FOR_API = {"overall", "7day", "1month", "3month", "6month", "12month"};
     public static final String[] DATE_PERIODS_FOR_TAB_NAMES = {"Overall", "Week", "Month", "3 months", "6 months", "Year"};
-    public static final String DATABASE_SCROBBLES_TABLE = "Scrobbles";
+
+    public static final String API_NO_ERROR = "No error";
+
+    public static final String DIALOG_FRAGMENT_KEY = "DialogFragment";
+    public static final Long DATE_LONG_DEFAUT_VALUE = -1L;
+    public static final Long DATE_LASTFM_LAUNCHED = 1016582400L;
+
+    public class DatabaseConstants {
+        public static final String DATABASE_SCROBBLES_TABLE = "Scrobbles";
+        public static final String DATABASE_TOP_ALBUMS_TABLE = "TopAlbums";
+        public static final String DATABASE_TOP_ARTISTS_TABLE = "TopArtists";
+        public static final String DATABASE_TOP_TRACKS_TABLE = "TopTracks";
+        public static final String COLUMN_ID = "_id";
+        public static final String COLUMN_TRACK = TRACK_KEY;
+        public static final String COLUMN_ARTIST = ARTIST_KEY;
+        public static final String COLUMN_ALBUM = ALBUM_KEY;
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_IMAGEURI = "imageUri";
+        public static final String COLUMN_RANK = "rank";
+        public static final String COLUMN_PLAYCOUNT = "playcount";
+        public static final String COLUMN_PERIOD = "period";
+    }
 }
--- app/src/main/java/by/d1makrat/library_fm/broadcast_receiver/NetworkStateReceiver.java
+++ app/src/main/java/by/d1makrat/library_fm/broadcast_receiver/NetworkStateReceiver.java
@@ -5,14 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-import by.d1makrat.library_fm.HttpsClient;
+import by.d1makrat.library_fm.https.HttpsClient;
 import by.d1makrat.library_fm.R;
 
 public class NetworkStateReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (!HttpsClient.isNetworkAvailable())
+        if (!HttpsClient.isNetworkAvailable()) {
             Toast.makeText(context, context.getResources().getString(R.string.offline_mode_message), Toast.LENGTH_SHORT).show();
+        }
     }
 }
