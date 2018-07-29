98435481f345b5c456fe66cedb00ff0349e9e68a
DzmitryLakisau <laki_dimon@mail.ru>
2018-07-29 19:13:28 +0300

    changed tests for Retrofit

--- app/src/test/java/by/d1makrat/library_fm/RetrofitCallTest.kt
+++ app/src/test/java/by/d1makrat/library_fm/RetrofitCallTest.kt
@@ -2,12 +2,14 @@ package by.d1makrat.library_fm
 
 import by.d1makrat.library_fm.Constants.API_BASE_URL
 import by.d1makrat.library_fm.Constants.DATE_PERIODS_FOR_API
+import by.d1makrat.library_fm.https.AdditionalParametersInterceptor
 import by.d1makrat.library_fm.https.LastFmRestApiService
 import by.d1makrat.library_fm.json.*
 import by.d1makrat.library_fm.json.model.ArtistsJsonModel
 import by.d1makrat.library_fm.json.model.ScrobblesJsonModel
 import by.d1makrat.library_fm.model.*
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import org.junit.Assert.assertTrue
 import org.junit.Assert.fail
 import org.junit.Before
@@ -28,6 +30,7 @@ class RetrofitCallTest {
     fun setUp(){
         val retrofit = Retrofit.Builder()
                 .baseUrl(API_BASE_URL)
+                .client(OkHttpClient().newBuilder().addInterceptor(AdditionalParametersInterceptor()).build())
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder()
                         .registerTypeAdapter(User::class.java, UserAdapter())
                         .registerTypeAdapter(TopAlbums::class.java, TopAlbumsAdapter())
@@ -35,9 +38,9 @@ class RetrofitCallTest {
                         .registerTypeAdapter(TopTracks::class.java, TopTracksAdapter())
                         .registerTypeAdapter(ArtistsJsonModel::class.java, SearchArtistResultsAdapter())
                         .registerTypeAdapter(ScrobblesJsonModel::class.java, ScrobblesAdapter())
+                        .registerTypeAdapter(SessionKey::class.java, SessionKeyAdapter())
+                        .registerTypeAdapter(SendScrobbleResult::class.java, SendScrobbleResultAdapter())
                         .create()))
-                .addConverterFactory(GsonConverterFactory.create(
-                        GsonBuilder().registerTypeAdapter(User::class.java, UserAdapter()).create()))
                 .build()
 
         mLastFmRestApiService = retrofit.create(LastFmRestApiService::class.java)
@@ -61,7 +64,7 @@ class RetrofitCallTest {
 
             assert(scrobbles?.size == countOfObjectInResponse)
 
-        } catch (e: IOException) {
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
@@ -186,6 +189,67 @@ class RetrofitCallTest {
             assertTrue(artists?.size == countOfObjectInResponse)
 
         } catch (e: Exception) {
+            e.printStackTrace()
+            fail()
+        }
+    }
+
+    @Test
+    fun getSessionKey(){
+        var sessionKey: SessionKey? = null
+
+        val call = mLastFmRestApiService.getSessionKey(user, BuildConfig.PASSWORD)
+
+        try {
+            val response = call.execute()
+
+            if (response.isSuccessful){
+                sessionKey = response.body()
+            }
+            else {
+               val mGson = GsonBuilder().registerTypeAdapter(SessionKey::class.java, SessionKeyAdapter()).create()
+
+                sessionKey = mGson.fromJson(response.errorBody()!!.string(), SessionKey::class.java)
+            }
+
+            assertTrue(sessionKey?.key != null)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            fail()
+        }
+    }
+
+    @Test
+    fun sendScrobble(){
+        var sendScrobbleResult: SendScrobbleResult? = null
+        var sessionKey = "null"
+
+        val artist = "Test"
+        val track = "Test"
+        val timestamp: Int = (System.currentTimeMillis() / 1000).toInt()
+
+        val call = mLastFmRestApiService.getSessionKey(user, BuildConfig.PASSWORD)
+
+        try{
+            val response = call.execute()
+
+            if (response.isSuccessful){
+                sessionKey = response.body()!!.key
+            }
+
+            val call2 = mLastFmRestApiService.sendScrobble(sessionKey, track, artist, null, null,
+                    null, timestamp)
+
+            val response2 = call2.execute()
+
+            if (response2.isSuccessful){
+                sendScrobbleResult = response2.body()
+            }
+
+            assertTrue(response2.isSuccessful && sendScrobbleResult?.code == 0)
+        }
+        catch (e: Exception){
             e.printStackTrace()
             fail()
         }
